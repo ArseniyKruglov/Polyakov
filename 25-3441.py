c038bf967c8716dea287665baa6a21baac4d7052
1Y <arseniy.krugloff@gmail.com>
2021-03-12 12:35:08 +0300

    Update 25-3441.py

--- 25-3441.py
+++ 25-3441.py
@@ -1,26 +1,22 @@
-import math
+# Для интервала [33333;55555] найдите числа, которые кратны сумме своих простых делителей. В качестве ответа приведите числа, для которых сумма простых делителей больше 250, – сначала найденное число, затем сумму его простых делителей. Примечание: само число в качестве делителя не учитывается.
 
 primes = []
 for i in range(2, 55555 + 1):
-    ok = True
-    
-    for j in range(2, i):
-        if (i % j == 0):
-            ok = False
+    for j in range(2, int(i ** 0.5) + 1):
+        if i % j == 0:
             break
-        
-    if (ok):
+    if else:
         primes.append(i)
 
 for i in range(33333, 55555 + 1):
-    local = []
+    prime_dividers = []
     for prime in primes:
-        if (i % prime == 0 and prime != i):
-            local.append(prime)
+        if (i % prime == 0 and prime <= int(i ** 0.5)):
+            prime_dividers.append(prime)
 
     sum = 0;
-    for element in local:
-        sum += element;
+    for prime_divider in prime_dividers:
+        sum += prime_divider;
 
     if (sum > 250 and i % sum == 0):
         print(i, sum)
