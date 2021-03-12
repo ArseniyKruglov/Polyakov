73f141bca1269468c8789c90ffea0c4cebd1cf5a
1Y <arseniy.krugloff@gmail.com>
2021-03-12 11:47:21 +0300

    Update 24-2525.py

--- 24-2525.py
+++ 24-2525.py
@@ -1,16 +1,20 @@
+# Текстовый файл k8-10.txt состоит не более чем из 106 символов. Определите максимальное количество идущих подряд символов, среди которых каждые два соседних различны.
+
 file = open('Source/24-1.txt', 'r')
 temp = ''
 max = 0
 
 for char in file.readline():
-    if (char in '0123456789'):
+    if temp != '':
+        print(temp)
+        
+    if char in '0123456789':
         temp += char
     else:
         if (temp != ''):
             temp = int(temp)
-            if (temp % 2 == 0):
-                if (temp > max):
-                    max = temp
+            if temp % 2 == 0 and temp > max:
+                max = temp
 
         temp = ''
 
