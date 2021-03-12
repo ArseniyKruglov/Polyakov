ff172d9a7e8943fe980f0c9b84f66f0cb15a8ffd
1Y <arseniy.krugloff@gmail.com>
2021-03-12 14:20:08 +0300

    Update 25-2928.py

--- 25-2928.py
+++ 25-2928.py
@@ -1,15 +1,12 @@
-import math
-
 for i in range (126849, 126871 + 1):
     dividers = []
     dividers.append(1)
-    dividers.append(i)
-    
-    for j in range (2, int(math.sqrt(i)) + 1):
-        if (i % j == 0):
+    for j in range (2, int(i ** 0.5) + 1):
+        if i % j == 0:
             dividers.append(j)
             dividers.append(i // j)
+    dividers.append(i)
 
-    if (len(dividers) == 4):
+    if len(dividers) == 4:
         dividers.sort()
         print(dividers[2], dividers[3])
