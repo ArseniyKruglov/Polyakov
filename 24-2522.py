a15db6c1e81b8263b51eeefca83857d2b804835f
1Y <arseniy.krugloff@gmail.com>
2021-03-12 11:35:38 +0300

    Update 24-2522.py

--- 24-2522.py
+++ 24-2522.py
@@ -1,15 +1,17 @@
-file = open('k8-10.txt', 'r')
+# Текстовый файл k8-10.txt состоит не более чем из 106 символов. Определите максимальное количество идущих подряд символов, среди которых каждые два соседних различны.
+
+file = open('Source/k8-10.txt', 'r')
 string = file.readline()
-length = 0
+length = 1
 max = 0
 
 for i in range (0, len(string) - 1):
     if (string[i] != string[i + 1]):
         length += 1
     else:
-        length = 0
+        length = 1
 
     if (length > max):
         max = length
 
-print(max + 1)
+print(max)
