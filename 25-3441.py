8fdbc1e6f9f91ab62ebafdf93c66433fd869132e
1Y <arseniy.krugloff@gmail.com>
2021-03-12 12:37:23 +0300

    Update 25-3441.py

--- 25-3441.py
+++ 25-3441.py
@@ -5,18 +5,21 @@ for i in range(2, 55555 + 1):
     for j in range(2, int(i ** 0.5) + 1):
         if i % j == 0:
             break
-    if else:
+    else:
         primes.append(i)
 
 for i in range(33333, 55555 + 1):
     prime_dividers = []
     for prime in primes:
-        if (i % prime == 0 and prime <= int(i ** 0.5)):
+        if prime > int(i ** 0.5):
+            break
+        
+        if i % prime == 0:
             prime_dividers.append(prime)
 
     sum = 0;
     for prime_divider in prime_dividers:
         sum += prime_divider;
 
-    if (sum > 250 and i % sum == 0):
+    if sum > 250 and i % sum == 0:
         print(i, sum)
