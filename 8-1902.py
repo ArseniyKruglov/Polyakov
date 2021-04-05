1b77fef35df40bea2c4fdbfd75c2058e4c4a7c2c
1Y <arseniy.krugloff@gmail.com>
2021-04-06 00:37:24 +0300

    Add files via upload

---
+++ 8-1902.py
@@ -0,0 +1,20 @@
+c = 0
+
+for i in range(1, 100000):
+    i = hex(i)[2:]
+
+    if len(i) == 4:
+        ok = True
+        
+        if len(set(i)) < 4:
+            ok = False
+        
+        for j in range(0, len(i) - 1):
+            if int(i[j], 16) % 2 == int(i[j + 1], 16) % 2:
+                ok = False
+                break
+                
+        if ok:
+            c += 1
+
+print(c)
